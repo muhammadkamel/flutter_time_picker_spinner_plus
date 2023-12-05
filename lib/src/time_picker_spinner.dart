6ed2c09b4f16b1ddc3b3ec1c1ad5d903c04f9d6f
Muhammad Kamel <140485297+totplatformflutter@users.noreply.github.com>
2023-12-06 00:48:24 +0200

    Init project

--- lib/src/time_picker_spinner.dart
+++ lib/src/time_picker_spinner.dart
@@ -1,69 +1,7 @@
 library time_picker_spinner;
 
 import 'package:flutter/material.dart';
-import 'dart:math';
-
-class ItemScrollPhysics extends ScrollPhysics {
-  /// Creates physics for snapping to item.
-  /// Based on PageScrollPhysics
-  final double? itemHeight;
-  final double targetPixelsLimit;
-
-  const ItemScrollPhysics({
-    ScrollPhysics? parent,
-    this.itemHeight,
-    this.targetPixelsLimit = 3.0,
-  })  : assert(itemHeight != null && itemHeight > 0),
-        super(parent: parent);
-
-  @override
-  ItemScrollPhysics applyTo(ScrollPhysics? ancestor) {
-    return ItemScrollPhysics(
-        parent: buildParent(ancestor), itemHeight: itemHeight);
-  }
-
-  double _getItem(ScrollPosition position) {
-    double maxScrollItem =
-        (position.maxScrollExtent / itemHeight!).floorToDouble();
-    return min(max(0, position.pixels / itemHeight!), maxScrollItem);
-  }
-
-  double _getPixels(ScrollPosition position, double item) {
-    return item * itemHeight!;
-  }
-
-  double _getTargetPixels(
-      ScrollPosition position, Tolerance tolerance, double velocity) {
-    double item = _getItem(position);
-    if (velocity < -tolerance.velocity)
-      item -= targetPixelsLimit;
-    else if (velocity > tolerance.velocity) item += targetPixelsLimit;
-    return _getPixels(position, item.roundToDouble());
-  }
-
-  @override
-  Simulation? createBallisticSimulation(
-      ScrollMetrics position, double velocity) {
-    // If we're out of range and not headed back in range, defer to the parent
-    // ballistics, which should put us back in range at a item boundary.
-//    if ((velocity <= 0.0 && position.pixels <= position.minScrollExtent) ||
-//        (velocity >= 0.0 && position.pixels >= position.maxScrollExtent))
-//      return super.createBallisticSimulation(position, velocity);
-    Tolerance tolerance = this.tolerance;
-    final double target =
-        _getTargetPixels(position as ScrollPosition, tolerance, velocity);
-    if (target != position.pixels)
-      return ScrollSpringSimulation(spring, position.pixels, target, velocity,
-          tolerance: tolerance);
-    return null;
-  }
-
-  @override
-  bool get allowImplicitScrolling => false;
-}
-
-typedef SelectedIndexCallback = void Function(int);
-typedef TimePickerCallback = void Function(DateTime);
+import 'package:time_picker_spinner_plus/src/item_scroll_physics.dart';
 
 class TimePickerSpinner extends StatefulWidget {
   final DateTime? time;
@@ -80,8 +18,8 @@ class TimePickerSpinner extends StatefulWidget {
   final bool isForce2Digits;
   final TimePickerCallback? onTimeChange;
 
-  TimePickerSpinner(
-      {Key? key,
+  const TimePickerSpinner(
+      {super.key,
       this.time,
       this.minutesInterval = 1,
       this.secondsInterval = 1,
@@ -94,14 +32,13 @@ class TimePickerSpinner extends StatefulWidget {
       this.alignment,
       this.spacing,
       this.isForce2Digits = false,
-      this.onTimeChange})
-      : super(key: key);
+      this.onTimeChange});
 
   @override
-  _TimePickerSpinnerState createState() => _TimePickerSpinnerState();
+  TimePickerSpinnerState createState() => TimePickerSpinnerState();
 }
 
-class _TimePickerSpinnerState extends State<TimePickerSpinner> {
+class TimePickerSpinnerState extends State<TimePickerSpinner> {
   ScrollController hourController = ScrollController();
   ScrollController minuteController = ScrollController();
   ScrollController secondController = ScrollController();
@@ -118,9 +55,9 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
 
   /// default settings
   TextStyle defaultHighlightTextStyle =
-      TextStyle(fontSize: 32, color: Colors.black);
+      const TextStyle(fontSize: 32, color: Colors.black);
   TextStyle defaultNormalTextStyle =
-      TextStyle(fontSize: 32, color: Colors.black54);
+      const TextStyle(fontSize: 32, color: Colors.black54);
   double defaultItemHeight = 60;
   double defaultItemWidth = 45;
   double defaultSpacing = 20;
@@ -129,15 +66,11 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
   /// getter
 
   TextStyle? _getHighlightedTextStyle() {
-    return widget.highlightedTextStyle != null
-        ? widget.highlightedTextStyle
-        : defaultHighlightTextStyle;
+    return widget.highlightedTextStyle ?? defaultHighlightTextStyle;
   }
 
   TextStyle? _getNormalTextStyle() {
-    return widget.normalTextStyle != null
-        ? widget.normalTextStyle
-        : defaultNormalTextStyle;
+    return widget.normalTextStyle ?? defaultNormalTextStyle;
   }
 
   int _getHourCount() {
@@ -153,19 +86,19 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
   }
 
   double? _getItemHeight() {
-    return widget.itemHeight != null ? widget.itemHeight : defaultItemHeight;
+    return widget.itemHeight ?? defaultItemHeight;
   }
 
   double? _getItemWidth() {
-    return widget.itemWidth != null ? widget.itemWidth : defaultItemWidth;
+    return widget.itemWidth ?? defaultItemWidth;
   }
 
   double? _getSpacing() {
-    return widget.spacing != null ? widget.spacing : defaultSpacing;
+    return widget.spacing ?? defaultSpacing;
   }
 
   AlignmentGeometry? _getAlignment() {
-    return widget.alignment != null ? widget.alignment : defaultAlignment;
+    return widget.alignment ?? defaultAlignment;
   }
 
   bool isLoop(int value) {
@@ -187,7 +120,7 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
 
   @override
   void initState() {
-    currentTime = widget.time == null ? DateTime.now() : widget.time;
+    currentTime = widget.time ?? DateTime.now();
 
     currentSelectedHourIndex =
         (currentTime!.hour % (widget.is24HourMode ? 24 : 12)) + _getHourCount();
@@ -218,7 +151,7 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
     super.initState();
 
     if (widget.onTimeChange != null) {
-      WidgetsBinding.instance!
+      WidgetsBinding.instance
           .addPostFrameCallback((_) => widget.onTimeChange!(getDateTime()));
     }
   }
@@ -299,7 +232,7 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
   }
 
   Widget spacer() {
-    return Container(
+    return SizedBox(
       width: _getSpacing(),
       height: _getItemHeight()! * 3,
     );
@@ -318,7 +251,7 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
     /// flutter: Another exception was thrown: 'package:flutter/src/widgets/scrollable.dart': Failed assertion: line 469 pos 12: '_hold == null || _drag == null': is not true.
     /// maybe later we can find out why this error is happening
 
-    Widget _spinner = NotificationListener<ScrollNotification>(
+    Widget spinner = NotificationListener<ScrollNotification>(
       onNotification: (scrollNotification) {
         if (scrollNotification is UserScrollNotification) {
           if (scrollNotification.direction.toString() ==
@@ -386,7 +319,7 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
 
     return Stack(
       children: <Widget>[
-        Positioned.fill(child: _spinner),
+        Positioned.fill(child: spinner),
         isScrolling
             ? Positioned.fill(
                 child: Container(
@@ -398,7 +331,7 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
   }
 
   Widget apSpinner() {
-    Widget _spinner = NotificationListener<ScrollNotification>(
+    Widget spinner = NotificationListener<ScrollNotification>(
       onNotification: (scrollNotification) {
         if (scrollNotification is UserScrollNotification) {
           if (scrollNotification.direction.toString() ==
@@ -442,7 +375,7 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
 
     return Stack(
       children: <Widget>[
-        Positioned.fill(child: _spinner),
+        Positioned.fill(child: spinner),
         isAPScrolling ? Positioned.fill(child: Container()) : Container()
       ],
     );
---
+++ lib/time_picker_spinner_plus.dart
@@ -0,0 +1,4 @@
+library time_picker_spinner_plus;
+
+export 'src/item_scroll_physics.dart';
+export 'src/time_picker_spinner.dart';
